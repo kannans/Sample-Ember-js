8e01f03d9f6972163bca7393b5430bfefffda72e
shivareddy547 <shiva.r547@gmail.com>
2014-03-05 18:06:30 +0530

    commit files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140226164511) do
+ActiveRecord::Schema.define(:version => 20140304111049) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
@@ -20,11 +20,20 @@ ActiveRecord::Schema.define(:version => 20140226164511) do
     t.float    "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "employees", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
