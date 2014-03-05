bbfb198995184b36dd65d7de2c8eb502ac324214
shivareddy547 <shiva.r547@gmail.com>
2014-03-05 21:08:16 +0530

    commit files for image validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :book_ispn, :name, :price,:image
-
+ validates :name,:author,:book_ispn,:price, :presence => true 
  def image_url
 	"/app/assets/"+image
  end
